f9c481ab890cb15fae7174f83e4ad5df9fd6ff98
KHALED SHAABAN <77199234+Khaled9810@users.noreply.github.com>
2023-04-06 05:57:25 +0200

    Update DIO.h

--- DIO Driver/DIO.h
+++ DIO Driver/DIO.h
@@ -35,7 +35,7 @@ uint8 DIO_GetPinDirection(uint8 port,uint8 pin_num);
 
 void DIO_SetPortValue(uint8 port,uint8 value);
 void DIO_SetPortDirection(uint8 port,uint8 direction);
-void DIO_GetPortValue(uint8 port,uint8* ptr_value);
+void DIO_GetPortValue(uint8 port,volatile uint8* ptr_value);
 void DIO_GetPortDirection(uint8 port,volatile uint8* ptr_value);
 
 
